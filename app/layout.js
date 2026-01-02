5cdee38c05c6886f3b79b7d8efeb3adb8e5ffe0c
OlekZakhar <olekzakhar@gmail.com>
2026-01-02 19:41:28 +0200

    [+] Додав шрифт до проекту і оптимізвав його

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// app/layout
+
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const dmSans = DM_Sans({
+  subsets: ["latin", "cyrillic"],
+  variable: "--font-dm-sans",
+  weight: ["400", "500", "600", "700", "800"],
+  style: ["normal"],
+  display: "swap",
+  preload: true,
+  fallback: ['system-ui', 'sans-serif']
 });
 
 export const metadata = {
@@ -19,9 +21,7 @@ export const metadata = {
 export default function RootLayout({ children, modal }) {
   return (
     <html lang="uk">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${dmSans.variable} antialiased`}>
         {children}
         {modal}
       </body>

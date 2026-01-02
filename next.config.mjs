2fb9d939797b417d646f3cc6f376c3f9d72833fd
OlekZakhar <olekzakhar@gmail.com>
2026-01-02 19:47:52 +0200

    [~] Додав у налаштування Next.js формати зображення, щоб при білді конвертувало зображення в ці формати і вони ставали легшими і швидше сторінки завантажувались

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  // Потрібно це для того, що при білді Next.js конвертне всі зображення в один з цих форматів
+  // і вони стануть легшими і будуть швидше завантажуватись
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
 };
 
 export default nextConfig;

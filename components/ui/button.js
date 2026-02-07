b882b4dde5ad1bd40a46c01ef26940cd651f28b6
OlekZakhar <olekzakhar@gmail.com>
2026-02-07 02:31:52 +0200

    [~] Пофіксив прибравши відсупи у ring щоб був просто як бордер у елементів інпуту, кнопок, текстареа, селект та інших

--- components/ui/button.js
+++ components/ui/button.js
@@ -7,7 +7,7 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-[10px] text-sm font-semibold ring-offset-background transition-all duration-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-[10px] text-sm font-semibold transition-all duration-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {

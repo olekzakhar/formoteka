7214ce0fa3fc815f77062a5306aef19792412430
OlekZakhar <olekzakhar@gmail.com>
2026-02-07 20:54:02 +0200

    [~] Додав елемент slider radix. Змінив стилі чи пофіксив елементи форми і інпути, текстареа, селект, label

--- components/ui/textarea.js
+++ components/ui/textarea.js
@@ -6,7 +6,7 @@ const Textarea = React.forwardRef(({ className, ...props }, ref) => {
   return (
     <textarea
       className={cn(
-        "flex min-h-[80px] w-full rounded-md border border-input bg-input px-3 py-2 text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+        "flex min-h-[90px] w-full rounded-md border border-input bg-input px-3 py-2 text-base placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
         className,
       )}
       ref={ref}
